8f582e7b256c04d6b23fc3fd2410f0666097bd7a
Renato Massaro <r@ena.to>
2024-07-27 13:01:41 -0300

    Fix unused reference in Webserver.Application

--- webserver/lib/webserver/application.ex
+++ webserver/lib/webserver/application.ex
@@ -19,11 +19,16 @@ defmodule Webserver.Application do
   Use this function when you need to recompile the Webserver routes in runtime.
   """
   def recompile do
-    dispatch = :cowboy_router.compile([{:_, routes()}])
+    routes =
+      Application.get_env(:webserver, :routes, [])
+      |> Enum.map(fn {route, opts} -> {route, Webserver.Dispatcher, opts} end)
+
+    dispatch = :cowboy_router.compile([{:_, routes}])
     :persistent_term.put(@dispatch_table, dispatch)
   end
 
   defp webserver_spec do
+    # TODO: DRY routes (also used in `recompile/0` above)
     routes =
       Application.get_env(:webserver, :routes, [])
       |> Enum.map(fn {route, opts} -> {route, Webserver.Dispatcher, opts} end)
