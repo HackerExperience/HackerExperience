c6950f11faf2923749eb1eeecd79316b47b6ffdc
Renato Massaro <r@ena.to>
2024-08-04 15:36:59 -0300

    Add tests to openapi.generate_schemas Mix task

---
+++ helix/lib/mix/task/openapi/generate_schemas.ex
@@ -0,0 +1,65 @@
+defmodule Mix.Tasks.Openapi.GenerateSchemas do
+  use Mix.Task
+  require Logger
+
+  alias Webserver.OpenApi.Spec.Generator, as: SpecGenerator
+
+  @default_target_dir "priv/openapi"
+
+  @doc """
+  Generates the OpenAPI schemas for Lobby.
+
+  Usage:
+  > mix openapi.generate_schemas [--target-dir priv/openapi]
+  """
+  @impl Mix.Task
+  def run(raw_args) do
+    {args, _} = OptionParser.parse!(raw_args, strict: [target_dir: :string])
+
+    case :timer.tc(fn -> do_run(args) end) do
+      {t, :ok} ->
+        Logger.info("OpenAPI specs written in #{trunc(t / 1000)}ms")
+
+      {_, reason} ->
+        Logger.error("Failed to generate specs: #{inspect(reason)}")
+        exit(:error)
+    end
+  end
+
+  defp do_run(args) do
+    target_dir = get_target_dir(args)
+    setup_env(target_dir)
+    generate_openapi_spec_file(:lobby, target_dir)
+  end
+
+  defp setup_env(target_dir) do
+    File.mkdir_p!(target_dir)
+  end
+
+  defp generate_openapi_spec_file(:lobby, target_dir) do
+    Lobby.Webserver.spec()
+    |> SpecGenerator.generate()
+    |> write_spec(:lobby, target_dir)
+  end
+
+  defp write_spec(spec, name, target_dir) do
+    path = Path.join(target_dir, "#{name}.json")
+    File.write!(path, :json.encode(spec))
+    generate_yaml_version(path)
+  end
+
+  defp generate_yaml_version(json_path) do
+    with {_, 1} <- System.cmd("which", ["yq"]),
+         do: raise("You need `yq` installed in your system")
+
+    yaml_path = String.replace(json_path, ".json", ".yaml")
+
+    case System.shell("cat #{json_path} | yq -y > #{yaml_path}") do
+      {_, 0} -> :ok
+      {error, _} -> {:error, error}
+    end
+  end
+
+  defp get_target_dir(args),
+    do: Keyword.get(args, :target_dir, @default_target_dir)
+end
