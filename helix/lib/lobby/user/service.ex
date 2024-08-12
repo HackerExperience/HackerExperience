9bae76e3a9c9efb676923c7355732193b1916c08
Renato Massaro <r@ena.to>
2024-08-11 21:02:31 -0300

    Refactor Svc.User with new Core.Fetch API

--- helix/lib/lobby/user/service.ex
+++ helix/lib/lobby/user/service.ex
@@ -1,35 +1,23 @@
 defmodule Lobby.Services.User do
-  # TODO: Rethink API so it becomes deep and narrow instead of shallow and wide
-
   alias DBLite, as: DB
   alias Lobby.User
 
-  # TODO: Rethink API so it becomes deep and narrow instead of shallow and wide
-  def fetch_by_email(email) do
-    DB.one({:users, :get_by_email}, [email])
-  end
-
-  def email_taken?(email) do
-    # TODO: Use custom fields and/or count(*)
-
-    case DB.one({:users, :get_by_email}, [email]) do
-      nil -> false
-      %{} -> true
-    end
-  end
-
-  def username_taken?(username) do
-    # TODO: Use custom fields and/or count(*)
-
-    case DB.one({:users, :get_by_username}, [username]) do
-      nil -> false
-      %{} -> true
-    end
-  end
+  # Operations
 
   def create(params) do
     params
     |> User.new()
     |> DB.insert()
+  end
+
+  # Queries
+
+  def fetch(filter_params, opts \\ []) do
+    filters = [
+      by_email: {:one, {:users, :get_by_email}},
+      by_username: {:one, {:users, :get_by_username}}
+    ]
+
+    Core.Fetch.query(filter_params, opts, filters)
   end
 end
