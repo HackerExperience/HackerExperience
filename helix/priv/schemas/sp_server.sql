3224e1142e593fc740bdb1593df415d723219e8b
Renato Massaro <r@ena.to>
2024-11-09 13:41:04 -0300

    Update SQL schema from older change
    Not sure when this is from, but it's an unrelated change that was left
    lying around in my local repository...
    
    Maybe we can enforce that schema dumps are up-to-date using a CI check?
    That would be very similar to how we enforce the _list_ of schemas.

--- helix/priv/schemas/sp_server.sql
+++ helix/priv/schemas/sp_server.sql
@@ -12,5 +12,5 @@ CREATE TABLE __db_migrations_summary (
   PRIMARY KEY (domain, version)
 ) STRICT;
 INSERT INTO __db_migrations_summary VALUES('server',241020084800);
-CREATE TABLE logs ( id INTEGER PRIMARY KEY, revision_id INTEGER NOT NULL, type TEXT NOT NULL, data TEXT NOT NULL, inserted_at TEXT NOT NULL, UNIQUE (id, revision_id) ) STRICT;
+CREATE TABLE logs ( id INTEGER, revision_id INTEGER NOT NULL, type TEXT NOT NULL, data TEXT NOT NULL, inserted_at TEXT NOT NULL, UNIQUE (id, revision_id) ) STRICT;
 COMMIT;
