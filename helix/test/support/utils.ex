bc4a73bc7e6c134f9447d4fbb917b2e0057180b6
Renato Massaro <r@ena.to>
2024-11-10 06:56:58 -0300

    Add SSEListener test util
    This will be useful to ease the barrier of testing Publishable events.

--- helix/test/support/utils.ex
+++ helix/test/support/utils.ex
@@ -2,4 +2,5 @@ defmodule Test.Utils do
   alias __MODULE__, as: U
 
   defdelegate jwt_token(opts \\ []), to: U.Token, as: :generate
+  defdelegate start_sse_listener(ctx, player, opts \\ []), to: U.SSEListener, as: :start
 end
