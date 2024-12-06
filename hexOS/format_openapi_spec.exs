cc391eb5c0e9fd490a2c180c69787285989cec84
Renato Massaro <r@ena.to>
2024-12-05 21:01:11 -0300

    Simplify format_openapi_spec.exs script

--- hexOS/format_openapi_spec.exs
+++ hexOS/format_openapi_spec.exs
@@ -7,21 +7,21 @@ defmodule OpenAPI.Elm.Formatter do
   @files [
     "src/API/Events/Types.elm",
     "src/API/Events/Json.elm",
+    "src/API/Game/Types.elm",
+    "src/API/Game/Json.elm",
     "src/API/Game/Api.elm"
   ]
 
   @custom_types [
     :server_id,
+    :tunnel_id,
     :nip
   ]
 
   def format do
     Enum.each(@custom_types, fn type ->
       Logger.debug("Formatting custom type: #{type}")
-
-      Enum.each(@files, fn file ->
-        handle(type, file)
-      end)
+      Enum.each(@files, &handle(type, &1))
     end)
 
     Enum.each(@files, &elm_format/1)
@@ -31,67 +31,67 @@ defmodule OpenAPI.Elm.Formatter do
   # Handlers
   ##################################################################################################
 
-  # TODO: I may be able to simplify some of these operations
   defp handle(:nip, file) do
-    insert_import("import Game.Model.NIP as NIP exposing (NIP(..))", file)
-
-    # Types
-    sed("s/{ nip \: String/{ nip \: NIP/", file)
-    sed("s/, nip \: String/, nip \: NIP/", file)
-    sed("s/, source_nip \: String/, source_nip \: NIP/", file)
-    sed("s/, target_nip \: String/, target_nip \: NIP/", file)
-
-    # Json parsing
-    sed("/\"nip\"/!b;n;s/Json\.Decode\.string/#{json_dec(:nip)}/", file)
-    sed("/\"source_nip\"/!b;n;s/Json\.Decode\.string/#{json_dec(:nip)}/", file)
-    sed("/\"target_nip\"/!b;n;s/Json\.Decode\.string/#{json_dec(:nip)}/", file)
-
-    sed(
-      "s/\"nip\", Json\.Encode\.string rec\.nip/" <>
-        "\"nip\", #{json_enc(:nip)} rec\.nip)/",
-      file
-    )
-
-    sed(
-      "s/\"source_nip\", Json\.Encode\.string rec\.source_nip/" <>
-        "\"source_nip\", #{json_enc(:nip)} rec\.source_nip)/",
-      file
-    )
-
-    sed(
-      "s/\"target_nip\", Json\.Encode\.string rec\.target_nip/" <>
-        "\"target_nip\", #{json_enc(:nip)} rec\.target_nip)/",
-      file
-    )
-
-    # Path parameters
-    sed("s/, config\.params\.nip/, NIP\.toString config\.params\.nip/", file)
-    sed("s/, config\.params\.target_nip/, NIP\.toString config\.params\.target_nip/", file)
+    replace("nip", "String", "NIP", file)
+    replace("source_nip", "String", "NIP", file)
+    replace("target_nip", "String", "NIP", file)
   end
 
   defp handle(:server_id, file) do
-    insert_import("import Game.Model.ServerID as ServerID exposing (ServerID(..))", file)
+    replace("mainframe_id", "Int", "ServerID", file)
+  end
 
-    # Types
-    sed("s/, mainframe_id \: Int/, mainframe_id \: ServerID/", file)
+  defp handle(:tunnel_id, file) do
+    replace("tunnel_id", "Int", "TunnelID", file)
+  end
 
-    # Json parsing
-    sed("/\"mainframe_id\"/!b;n;s/Json\.Decode\.int/#{json_dec("ServerID", "int")}/", file)
+  ##################################################################################################
+  # Replacers
+  ##################################################################################################
 
+  defp replace(name, oas_type, elm_type, file) do
+    oas_decoder_type = String.downcase(oas_type)
+
+    # Make sure file has necessary imports
+    insert_import(elm_type, file)
+
+    # Replace type
+    sed("s/, #{name} \: #{oas_type}/, #{name} \: #{elm_type}/", file)
+    sed("s/{ #{name} \: #{oas_type}/{ #{name} \: #{elm_type}/", file)
+    sed("s/, #{name} \: Maybe #{oas_type}/, #{name} \: Maybe #{elm_type}/", file)
+    sed("s/{ #{name} \: Maybe #{oas_type}/{ #{name} \: Maybe #{elm_type}/", file)
+
+    # Replace Decoder
     sed(
-      "s/\"mainframe_id\", Json\.Encode\.int rec\.mainframe_id/" <>
-        "\"mainframe_id\", #{json_enc("ServerID", "int")} rec\.mainframe_id)/",
+      "/\"#{name}\"/!b;n;s/Json\.Decode\.#{oas_decoder_type}/" <>
+        "#{json_dec(elm_type, oas_decoder_type)}/",
       file
     )
+
+    # Replace Encoder
+    sed(
+      "s/\"#{name}\", Json\.Encode\.#{oas_decoder_type} rec\.#{name}/" <>
+        "\"#{name}\", #{json_enc(elm_type, oas_decoder_type)} rec\.#{name})/",
+      file
+    )
+
+    # Replace Encoder (Maybe variant)
+    sed(
+      "s/\"#{name}\", Json\.Encode\.#{oas_decoder_type} mapUnpack/" <>
+        "\"#{name}\", #{json_enc(elm_type, oas_decoder_type)} mapUnpack)/",
+      file
+    )
+
+    # Replace path parameters
+    # TODO: This will cause issues when path parameter is, say, LogRevisionID
+    sed("s/, config\.params\.#{name}/, #{elm_type}\.toString config\.params\.#{name}/", file)
   end
 
   ##################################################################################################
   # Utils
   ##################################################################################################
 
-  defp json_dec(elm_type, json_type \\ "int")
-
-  defp json_dec(:nip, _) do
+  defp json_dec("NIP", _) do
     "(Json\.Decode\.map (\\\\nip -> NIP\.fromString nip) Json\.Decode\.string)"
   end
 
@@ -99,9 +99,7 @@ defmodule OpenAPI.Elm.Formatter do
     "(Json\.Decode\.map #{elm_type} Json\.Decode\.#{json_type})"
   end
 
-  defp json_enc(elm_type, json_type \\ "int")
-
-  defp json_enc(:nip, _) do
+  defp json_enc("NIP", _) do
     "Json\.Encode\.string (NIP\.toString "
   end
 
@@ -109,16 +107,16 @@ defmodule OpenAPI.Elm.Formatter do
     "Json\.Encode\.#{json_type} (#{elm_type}\.toValue "
   end
 
-  defp insert_import(line, file) do
+  defp insert_import(name, file) do
+    line = "import Game.Model.#{name} as #{name} exposing (#{name}(..))"
+
     if not exists_in_file?(line, file) do
       if exists_in_file?("^import", file) do
         # Since this file already have imports, just add the import before the first one
-        # sed("0,/^import/s/#{line}\n//", file)
-
         sed("0,/^import/!b;//i#{line}", file)
       else
-        # This file does not have any imports, so just add it at the second line (after module)
-        sed("1 a #{line}\n", file)
+        # This file does not have any imports, so just add it after the first blank line
+        sed("0,/^$/!b;/^$/a#{line}", file)
       end
     end
   end
