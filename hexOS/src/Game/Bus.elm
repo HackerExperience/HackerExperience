28c01bf5f4ddfdd53adcda994d239baecb7dd14c
Renato Massaro <r@ena.to>
2024-12-15 09:01:39 -0300

    Add note about possible future refactor/rename
    I think it makes more sense for this module to be named State.Bus
    instead, but it's better to wait for it to be used in different ways so
    I can confirm my suspicions.

--- hexOS/src/Game/Bus.elm
+++ hexOS/src/Game/Bus.elm
@@ -1,5 +1,7 @@
 module Game.Bus exposing (Action(..))
 
+-- TODO: Maybe rename this to State.Bus?
+
 import Game.Model.NIP exposing (NIP)
 import Game.Model.ServerID exposing (ServerID)
 import Game.Universe exposing (Universe)
