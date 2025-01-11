c1a878bd70856d607d650470327a0f1887650e4c
Renato Massaro <r@ena.to>
2025-01-11 18:53:04 -0300

    Improve test
    It was using hard-coded and fragile/flaky definitions. Now it's using a
    more robust set of input, based on what the Executable actually
    returned.

--- helix/test/game/process/service_test.exs
+++ helix/test/game/process/service_test.exs
@@ -1,7 +1,6 @@
 defmodule Game.Services.ProcessTest do
   use Test.DBCase, async: true
   alias Game.Services, as: Svc
-  alias Game.Process.Log.Edit, as: LogEditProcess
   alias Game.Events.Process.Created, as: ProcessCreatedEvent
 
   setup [:with_game_db]
@@ -9,31 +8,17 @@ defmodule Game.Services.ProcessTest do
   describe "create/4" do
     test "creates a new process" do
       %{server: server, entity: entity} = Setup.server()
-      log = Setup.log!(server.id)
 
-      # TODO: This test is wrong, overall. I should generate the params the same way Executable does
-      registry_data =
-        %{
-          dynamic: [:cpu],
-          static: %{ram: 10},
-          objective: %{cpu: 1000},
-          tgt_log_id: log.id,
-          limit: %{}
-        }
-
-      # Using `LogEditProcess` as a sample process
-      process_data = LogEditProcess.new(%{type: :local_login, data: %{}}, %{})
-      process_type = LogEditProcess.get_process_type(%{}, %{})
-      process_info = {process_type, process_data}
+      spec = Setup.process_spec(server.id, entity.id, type: :log_edit)
 
       assert {:ok, process, [event]} =
-               Svc.Process.create(server.id, entity.id, registry_data, process_info)
+               Svc.Process.create(server.id, entity.id, spec.registry_data, spec.process_info)
 
       assert process.server_id == server.id
       assert process.entity_id == entity.id
-      assert process.data == process_data
-      assert process.type == process_type
-      assert process.registry == get_registry_params(registry_data)
+      assert process.data == spec.data
+      assert process.type == spec.type
+      assert process.registry == get_registry_params(spec.registry_data)
 
       assert event.data.__struct__ == ProcessCreatedEvent
       assert event.data.process == process
