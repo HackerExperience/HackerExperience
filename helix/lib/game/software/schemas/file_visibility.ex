5123aa3a2bc50fb43208d655195ccb5e08532813
Renato Massaro <r@ena.to>
2025-01-18 17:29:57 -0300

    Add Game.FileVisibility schema

---
+++ helix/lib/game/software/schemas/file_visibility.ex
@@ -0,0 +1,22 @@
+defmodule Game.FileVisibility do
+  use Core.Schema
+  alias Game.Entity
+
+  @context :player
+  @table :file_visibilities
+
+  @schema [
+    {:server_id, ID.ref(:server_id)},
+    {:file_id, ID.ref(:file_id)},
+    {:inserted_at, {:datetime_utc, [precision: :millisecond], mod: :inserted_at}},
+    {:entity_id, {ID.ref(:entity_id), virtual: true, after_read: :get_entity_id}}
+  ]
+
+  def new(params) do
+    params
+    |> Schema.cast()
+    |> Schema.create()
+  end
+
+  def get_entity_id(_, _, %{shard_id: raw_entity_id}), do: Entity.ID.from_external(raw_entity_id)
+end
--- helix/priv/schemas/sp_player.sql
+++ helix/priv/schemas/sp_player.sql
@@ -6,11 +6,13 @@ CREATE TABLE __db_migrations (
   PRIMARY KEY (domain, version)
 ) STRICT;
 INSERT INTO __db_migrations VALUES('player',241020094500);
+INSERT INTO __db_migrations VALUES('player',250118202152);
 CREATE TABLE __db_migrations_summary (
   domain TEXT,
   version INTEGER,
   PRIMARY KEY (domain, version)
 ) STRICT;
-INSERT INTO __db_migrations_summary VALUES('player',241020094500);
+INSERT INTO __db_migrations_summary VALUES('player',250118202152);
 CREATE TABLE log_visibilities ( server_id INTEGER, log_id INTEGER, revision_id INTEGER, inserted_at TEXT NOT NULL, PRIMARY KEY (server_id, log_id, revision_id) ) STRICT, WITHOUT ROWID;
+CREATE TABLE file_visibilities( server_id INTEGER, file_id INTEGER, inserted_at TEXT NOT NULL, PRIMARY KEY (server_id, file_id) ) STRICT, WITHOUT ROWID;
 COMMIT;
