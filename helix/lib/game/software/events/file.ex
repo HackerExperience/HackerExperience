15ff12ddba1e2178e378e3ae81492e3c2ae99e53
Renato Massaro <r@ena.to>
2025-01-24 21:20:29 -0300

    Stop calling `file.name` (not implemented yet)

--- helix/lib/game/software/events/file.ex
+++ helix/lib/game/software/events/file.ex
@@ -41,11 +41,12 @@ defmodule Game.Events.File do
         )
       end
 
-      def generate_payload(%{data: %{process: process, file: file, installation: installation}}) do
+      def generate_payload(%{data: %{process: process, file: _file, installation: installation}}) do
         payload =
           %{
             installation_id: installation.id,
-            file_name: file.name,
+            # file_name: file.name,
+            file_name: "todo",
             memory_usage: installation.memory_usage,
             process_id: process.id
           }
