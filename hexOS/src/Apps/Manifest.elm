45ade7dc21b32c69decc370beba1b6fdd1ad9dca
Renato Massaro <r@ena.to>
2025-03-02 11:23:06 -0300

    Remove commented out code

--- hexOS/src/Apps/Manifest.elm
+++ hexOS/src/Apps/Manifest.elm
@@ -9,10 +9,3 @@ type Manifest
       -- Popups
     | PopupConfirmationDialog
     | PopupDemoSingleton
-
-
-
--- -- NOTE: I may be able to merge both manifests into one (right?)
--- -- NOTE: Maybe move this to Popups.Manifest (i.e. its own root folder)
--- type PopupManifest
---     = ConfirmationDialog
