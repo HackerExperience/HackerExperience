8ea31cd52f185a10f31ac1a965eeb7fc24e9e4f1
Renato Massaro <r@ena.to>
2025-10-04 08:41:36 -0300

    Move FeebDB shards config to runtime.exs

--- helix/config/runtime.exs
+++ helix/config/runtime.exs
@@ -1,5 +1,36 @@
 import Config
 
+config :feebdb,
+  contexts: %{
+    lobby: %{
+      shard_type: :global
+    },
+    singleplayer: %{
+      shard_type: :global,
+      domains: [:game]
+    },
+    multiplayer: %{
+      shard_type: :global,
+      domains: [:game]
+    },
+    sp_player: %{
+      shard_type: :dedicated,
+      domains: [:player]
+    },
+    mp_player: %{
+      shard_type: :dedicated,
+      domains: [:player]
+    },
+    sp_server: %{
+      shard_type: :dedicated,
+      domains: [:server]
+    },
+    mp_server: %{
+      shard_type: :dedicated,
+      domains: [:server]
+    }
+  }
+
 config :helix, Lobby.Webserver, port: if(Mix.env() != :test, do: 4000, else: 5000)
 
 config :helix, Game.Webserver.Singleplayer,
--- helix/config/test.exs
+++ helix/config/test.exs
@@ -5,33 +5,4 @@ config :logger, level: :warning
 config :feebdb,
   data_dir: System.get_env("HELIX_TEST_DATA_DIR", "/he2_test"),
   migrations_dir: "priv/migrations",
-  contexts: %{
-    lobby: %{
-      shard_type: :global
-    },
-    singleplayer: %{
-      shard_type: :global,
-      domains: [:game]
-    },
-    multiplayer: %{
-      shard_type: :global,
-      domains: [:game]
-    },
-    sp_player: %{
-      shard_type: :dedicated,
-      domains: [:player]
-    },
-    mp_player: %{
-      shard_type: :dedicated,
-      domains: [:player]
-    },
-    sp_server: %{
-      shard_type: :dedicated,
-      domains: [:server]
-    },
-    mp_server: %{
-      shard_type: :dedicated,
-      domains: [:server]
-    }
-  },
   is_test_mode: true
