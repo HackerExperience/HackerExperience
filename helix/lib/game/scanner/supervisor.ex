f46467b5b0b97971216e8cfb7fd56162e3dfaaa9
Renato Massaro <r@ena.to>
2025-10-19 13:26:09 -0300

    Start Universe's TaskCompletionWorker iff not on Test env

--- helix/lib/game/scanner/supervisor.ex
+++ helix/lib/game/scanner/supervisor.ex
@@ -8,17 +8,24 @@ defmodule Game.Scanner.Supervisor do
   end
 
   def init(_) do
+    # Automatically start the TaskCompletionWorker for each universe, provided we are not running
+    # tests. Reason being `shard_id=1` does not exist in tests, which cover the worker by ad-hoc
+    # processes spawned within the context of each test.
     children =
-      [
-        %{
-          id: :task_completion_worker_sp,
-          start: {TaskCompletionWorker, :start_link, [[:singleplayer, 1]]}
-        },
-        %{
-          id: :task_completion_worker_mp,
-          start: {TaskCompletionWorker, :start_link, [[:multiplayer, 1]]}
-        }
-      ]
+      if Mix.env() != :test do
+        [
+          %{
+            id: :task_completion_worker_sp,
+            start: {TaskCompletionWorker, :start_link, [[:singleplayer, 1]]}
+          },
+          %{
+            id: :task_completion_worker_mp,
+            start: {TaskCompletionWorker, :start_link, [[:multiplayer, 1]]}
+          }
+        ]
+      else
+        []
+      end
 
     Supervisor.init(children, strategy: :one_for_one)
   end
