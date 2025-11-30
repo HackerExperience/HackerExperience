f4e6e115407bef536ebb900342d28caa436c7f5c
Renato Massaro <r@ena.to>
2025-11-29 22:33:41 -0300

    Add `HELIX_ROLE` for custom deployment behavior

--- helix/lib/game/scanner/supervisor.ex
+++ helix/lib/game/scanner/supervisor.ex
@@ -8,23 +8,30 @@ defmodule Game.Scanner.Supervisor do
   end
 
   def init(_) do
+    role = Helix.get_role()
+
+    singleplayer_child =
+      %{
+        id: :task_completion_worker_sp,
+        start: {TaskCompletionWorker, :start_link, [[:singleplayer, 1]]}
+      }
+
+    multiplayer_child =
+      %{
+        id: :task_completion_worker_mp,
+        start: {TaskCompletionWorker, :start_link, [[:multiplayer, 1]]}
+      }
+
     # Automatically start the TaskCompletionWorker for each universe, provided we are not running
-    # tests. Reason being `shard_id=1` does not exist in tests, which cover the worker by ad-hoc
+    # tests. Reason being `shard_id=1` does not exist in tests. Tests cover the worker by ad-hoc
     # processes spawned within the context of each test.
     children =
-      if Mix.env() != :test do
-        [
-          %{
-            id: :task_completion_worker_sp,
-            start: {TaskCompletionWorker, :start_link, [[:singleplayer, 1]]}
-          },
-          %{
-            id: :task_completion_worker_mp,
-            start: {TaskCompletionWorker, :start_link, [[:multiplayer, 1]]}
-          }
-        ]
-      else
-        []
+      case {role, Mix.env()} do
+        {:lobby, _} -> []
+        {_, :test} -> []
+        {:singleplayer, _} -> [singleplayer_child]
+        {:multiplayer, _} -> [multiplayer_child]
+        {:all, _} -> [singleplayer_child, multiplayer_child]
       end
 
     Supervisor.init(children, strategy: :one_for_one)
