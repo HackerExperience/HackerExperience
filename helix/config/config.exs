ce2fd40fc82dd04496eaeb9153353764246191a0
Renato Massaro <r@ena.to>
2025-12-06 08:09:54 -0300

    Add .env file

--- helix/config/config.exs
+++ helix/config/config.exs
@@ -1,5 +1,22 @@
 import Config
 
+read_env_file = fn path ->
+  if File.exists?(path) do
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.each(fn line ->
+      case String.split(line, "=", parts: 2) do
+        [key, value] -> System.put_env(key, value)
+        _ -> :noop
+      end
+    end)
+  end
+end
+
+# Source the .env/.local.env files
+Enum.each([".env", ".local.env"], read_env_file)
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
